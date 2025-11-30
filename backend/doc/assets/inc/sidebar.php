d1108dc49bed0e99d85a4bb4e4de61dc1bac4c3a
Muhammad Roshaan Idrees <roshaanidrees48@gmail.com>
2025-11-30 15:36:06 +0500

    updated

--- backend/doc/assets/inc/sidebar.php
+++ backend/doc/assets/inc/sidebar.php
@@ -9,6 +9,9 @@
 
                             <li class="menu-title">Navigation</li>
 
+                            <?php require_once(__DIR__ . '/permissions.php'); $dept = isset($_SESSION['dept']) ? $_SESSION['dept'] : null; ?>
+
+                            <?php if (doc_is_allowed_route($dept, 'his_doc_dashboard.php')): ?>
                             <li>
                                 <a href="his_doc_dashboard.php">
                                     <i class="fe-airplay"></i>
@@ -16,7 +19,15 @@
                                 </a>
                                 
                             </li>
+                            <?php endif; ?>
 
+                            <?php if (
+                                doc_is_allowed_route($dept, 'his_doc_register_patient.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_view_patients.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_manage_patient.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_discharge_patient.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_patient_transfer.php')
+                            ): ?>
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="fab fa-accessible-icon "></i>
@@ -24,27 +35,39 @@
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_doc_register_patient.php">Register Patient</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_view_patients.php">View Patients</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_manage_patient.php">Manage Patients</a>
-                                    </li>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_register_patient.php')): ?>
+                                    <li><a href="his_doc_register_patient.php">Register Patient</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_view_patients.php')): ?>
+                                    <li><a href="his_doc_view_patients.php">View Patients</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_manage_patient.php')): ?>
+                                    <li><a href="his_doc_manage_patient.php">Manage Patients</a></li>
+                                    <?php endif; ?>
                                     <hr>
-                                    <li>
-                                        <a href="his_doc_discharge_patient.php">Discharge Patients</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_patient_transfer.php">Patient Transfers</a>
-                                    </li>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_discharge_patient.php')): ?>
+                                    <li><a href="his_doc_discharge_patient.php">Discharge Patients</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_patient_transfer.php')): ?>
+                                    <li><a href="his_doc_patient_transfer.php">Patient Transfers</a></li>
+                                    <?php endif; ?>
                                 </ul>
                             </li>
+                            <?php endif; ?>
 
                           
 
+                            <?php if (
+                                doc_is_allowed_route($dept, 'his_doc_add_pharm_cat.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_view_pharm_cat.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_manage_pharm_cat.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_add_pharmaceuticals.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_view_pharmaceuticals.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_manage_pharmaceuticals.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_add_presc.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_view_presc.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_manage_presc.php')
+                            ): ?>
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="mdi mdi-pill"></i>
@@ -52,39 +75,44 @@
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_doc_add_pharm_cat.php">Add Pharm Category</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_view_pharm_cat.php">View Pharm Category</a>
-                                    </li>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_add_pharm_cat.php')): ?>
+                                    <li><a href="his_doc_add_pharm_cat.php">Add Pharm Category</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_view_pharm_cat.php')): ?>
+                                    <li><a href="his_doc_view_pharm_cat.php">View Pharm Category</a></li>
+                                    <?php endif; ?>
                                     <li>
                                         <a href="his_doc_manage_pharm_cat.php">Manage Pharm Category</a>
                                     </li>
                                     <hr>
-                                    <li>
-                                        <a href="his_doc_add_pharmaceuticals.php">Add Pharmaceuticals</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_view_pharmaceuticals.php">View Pharmaceuticals</a>
-                                    </li>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_add_pharmaceuticals.php')): ?>
+                                    <li><a href="his_doc_add_pharmaceuticals.php">Add Pharmaceuticals</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_view_pharmaceuticals.php')): ?>
+                                    <li><a href="his_doc_view_pharmaceuticals.php">View Pharmaceuticals</a></li>
+                                    <?php endif; ?>
                                     <li>
                                         <a href="his_doc_manage_pharmaceuticals.php">Manage Pharmaceuticals</a>
                                     </li>
                                     <hr>
-                                    <li>
-                                        <a href="his_doc_add_presc.php">Add Prescriptions</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_view_presc.php">View Prescriptions</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_manage_presc.php">Manage Prescriptions</a>
-                                    </li>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_add_presc.php')): ?>
+                                    <li><a href="his_doc_add_presc.php">Add Prescriptions</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_view_presc.php')): ?>
+                                    <li><a href="his_doc_view_presc.php">View Prescriptions</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_manage_presc.php')): ?>
+                                    <li><a href="his_doc_manage_presc.php">Manage Prescriptions</a></li>
+                                    <?php endif; ?>
                                 </ul>
                             </li>
+                            <?php endif; ?>
 
                             
+                            <?php if (
+                                doc_is_allowed_route($dept, 'his_doc_pharm_inventory.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_equipments_inventory.php')
+                            ): ?>
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class=" fas fa-funnel-dollar "></i>
@@ -92,18 +120,23 @@
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
-                                   
-                                    <li>
-                                        <a href="his_doc_pharm_inventory.php">Pharmaceuticals</a>
-                                    </li>
-
-                                    <li>
-                                        <a href="his_doc_equipments_inventory.php">Assets</a>
-                                    </li>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_pharm_inventory.php')): ?>
+                                    <li><a href="his_doc_pharm_inventory.php">Pharmaceuticals</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_equipments_inventory.php')): ?>
+                                    <li><a href="his_doc_equipments_inventory.php">Assets</a></li>
+                                    <?php endif; ?>
                                     
                                 </ul>
                             </li>
+                            <?php endif; ?>
                 
+                            <?php if (
+                                doc_is_allowed_route($dept, 'his_doc_patient_lab_test.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_patient_lab_result.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_patient_lab_vitals.php') ||
+                                doc_is_allowed_route($dept, 'his_doc_lab_report.php')
+                            ): ?>
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="mdi mdi-flask"></i>
@@ -111,24 +144,25 @@
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_doc_patient_lab_test.php">Patient Lab Tests</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_patient_lab_result.php">Patient Lab Results</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_patient_lab_vitals.php">Patient Vitals</a>
-                                    </li>
-                                    
-                                    <li>
-                                        <a href="his_doc_lab_report.php">Lab Reports</a>
-                                    </li>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_patient_lab_test.php')): ?>
+                                    <li><a href="his_doc_patient_lab_test.php">Patient Lab Tests</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_patient_lab_result.php')): ?>
+                                    <li><a href="his_doc_patient_lab_result.php">Patient Lab Results</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_patient_lab_vitals.php')): ?>
+                                    <li><a href="his_doc_patient_lab_vitals.php">Patient Vitals</a></li>
+                                    <?php endif; ?>
+                                    <?php if (doc_is_allowed_route($dept, 'his_doc_lab_report.php')): ?>
+                                    <li><a href="his_doc_lab_report.php">Lab Reports</a></li>
+                                    <?php endif; ?>
                                     <hr>
                                     
                                 </ul>
                             </li>
+                            <?php endif; ?>
 
+                            <?php if (doc_is_allowed_route($dept, 'his_doc_view_payrolls.php')): ?>
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="mdi mdi-cash-refund "></i>
@@ -142,6 +176,7 @@
                                     </li>
                                 </ul>
                             </li>
+                            <?php endif; ?>
 
                             
 
